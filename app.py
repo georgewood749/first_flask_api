de1b81c58ef29b7b76bcd357aa8f503ef65c5f0f
georgewood749 <georgewood749@gmail.com>
2022-11-22 11:26:15 +0000

    index, show actions added

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from werkzeug.exceptions import NotFound, InternalServerError, BadRequest
+
 
 app = Flask(__name__)
 CORS(app)
@@ -13,3 +14,41 @@ doggos = [
 ]
 
 
+@app.route("/")
+def hello_world():
+    return f"Hello world!"
+
+
+@app.route("/doggos", methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return jsonify(doggos)
+    elif request.method == "POST":
+        data = request.json
+        last_id = doggos[-1]["id"]
+        data["id"] = last_id + 1
+        doggos.append(data)
+        print(data)
+        return f"{data['name']} was added to the database.", 201
+
+
+@app.route("/doggos/<int:doggo_id>")
+def show(doggo_id):
+    try:
+        return next(dog for dog in doggos if dog['id'] == doggo_id), 302
+    except:
+        raise BadRequest(f"Could not find this doggo in the database.")
+
+
+@app.errorhandler(NotFound)
+def handle_404(err):
+    return jsonify({"message": f"Doggo not found. {err}"}), 404
+
+
+@app.errorhandler(InternalServerError)
+def handle_500(err):
+    return jsonify({"message": f"Internal server error"}), 500
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ crud-routes.md
@@ -0,0 +1,27 @@
+| **URL** | **HTTP VERB** | **Action**|
+|------------|------------|------------|
+| /photos/         | GET        | index  
+| /photos/new         | GET        | new   
+| /photos/         | POST        | create   
+| /photos/:id         | GET        | show         
+| /photos/:id/edit         | GET        | edit       
+| /photos/:id         | PATCH/PUT        | update    
+| /photos/:id         | DELETE        | destroy  
+
+## mantra
+get \
+get \
+post \
+get \
+get \
+patch \
+delete \
+
+## mnemonic 7 - crud routes
+i - index \
+should - show \
+not - new \
+call - create \
+ex - edit \
+upon - update \
+drinking - destroy \
